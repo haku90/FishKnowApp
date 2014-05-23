816174cc414a6c6b1a629c5b0079dbedd5472799
Adam Hakowski <adam.hakowski@gmail.com>
2014-05-23 17:46:54 +0200

    Update

--- FishKnow/FishKnow/View/DispFish.xaml.cs
+++ FishKnow/FishKnow/View/DispFish.xaml.cs
@@ -16,11 +16,24 @@ namespace FishKnow.View
         {
             DataContext = App.ViewModel1;
             InitializeComponent();
+            BackKeyPress += DispFish_BackKeyPress;
+        }
+
+        void DispFish_BackKeyPress(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            App.ViewModel1.select = null;
         }
 
         private void ApplicationBarMenuItem_Click_1(object sender, EventArgs e)
         {
-           
+            App.ViewModel1.select = null;
+            NavigationService.GoBack();
+        }
+
+        private void ApplicationBarMenuItem_Click_2(object sender, EventArgs e)
+        {
+            App.ViewModel1.select = null;
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
         }
     }
 }
